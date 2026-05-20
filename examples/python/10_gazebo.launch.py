117993e55fabf0e95d5fee577eb4366f6ef425db
Nikolas Dahn <nikolas.dahn@dfki.de>
2026-05-20 17:17:29 +0200

    New rosbag example, added use_sim_time to gazebo example

--- examples/python/10_gazebo.launch.py
+++ examples/python/10_gazebo.launch.py
@@ -12,15 +12,18 @@ def over_stimulation():
     bl = BetterLaunch()
     bl.logger.info("Gazebo version: %s", gazebo.get_gazebo_version())
 
-    gazebo.gazebo_launch("better_launch", "test.world")
-    gazebo.spawn_model("cube", bl.find("better_launch", "cube.sdf"))
-    gazebo.spawn_topic_bridge(
-        gazebo.GazeboBridge.clock_bridge(),
-        remaps={"/clock": "/gz_clock"},
-    )
-    gazebo.spawn_image_bridge(
-        gazebo.GazeboBridge("/camera", "sensor_msgs/msg/Image", "gz2ros")
-    )
-    gazebo.spawn_world_transform()
+    # Not relevant here, but you can set use_sim_time for all nodes within a group. This 
+    # setting will also be inherited by nested groups - unless they override it themselves!
+    with bl.group("sim", use_sim_time=True):
+        gazebo.gazebo_launch("better_launch", "test.world")
+        gazebo.spawn_model("cube", bl.find("better_launch", "cube.sdf"))
+        gazebo.spawn_topic_bridge(
+            gazebo.GazeboBridge.clock_bridge(),
+            remaps={"/clock": "/gz_clock"},
+        )
+        gazebo.spawn_image_bridge(
+            gazebo.GazeboBridge("/camera", "sensor_msgs/msg/Image", "gz2ros")
+        )
+        gazebo.spawn_world_transform()
 
     bl.logger.info("The loaded gazebo world is %s", gazebo.get_active_world_name())
---
+++ examples/python/13_rosbag.launch.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from fnmatch import fnmatch
+from better_launch import BetterLaunch, launch_this, convenience
+
+
+@launch_this
+def kneel_and_bag(pattern: str = "*topic*", test: int = 5):
+    """
+    This example mostly serves to raise awareness of one very nice convenience function :)
+    """
+    bl = BetterLaunch()
+
+    def should_record(topic: str) -> bool:
+        if not fnmatch(topic, pattern):
+            print("### nope", topic, pattern, pattern[0], type(pattern), type(topic))
+            return False
+
+        # Just some example code to select topics of type std_msgs/String
+        topics_and_types = dict(bl.shared_node.get_topic_names_and_types())
+        types = topics_and_types.get(topic, [])
+        print("###", topic, types, topics_and_types)
+        for tp in types:
+            if tp == "std_msgs/msg/String":
+                return True
+        
+        return False
+
+    bl.node(
+        "examples_rclpy_minimal_publisher",
+        "publisher_local_function",
+        "my_talker",
+    )
+
+    # Record all topics that fulfill the should_record predicate
+    convenience.record_topics(
+        None,
+        should_record,
+        max_bag_size = 0.1,  # split after 0.1 MB
+        recordings = 3,  # record 3 bags in total
+    )
