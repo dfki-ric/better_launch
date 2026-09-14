f468fed82dc414355a8f930aece02cb193483813
Nikolas Dahn <nikolas.dahn@dfki.de>
2026-09-14 13:39:30 +0200

    Minor cleanup

--- examples/python/14_gdb.launch.py
+++ examples/python/14_gdb.launch.py
@@ -5,7 +5,7 @@ from better_launch import BetterLaunch, launch_this
 @launch_this
 def stop_bugging_me():
     """
-    If you want to debug a node or execute it by some other means you can do so easily through the node's `exec_args`. These will be prepended to the node's run command.
+    If you want to debug a node or execute it by some other means you can do so easily through the node's `prefix_args`. These will be prepended to the node's run command.
     """
     bl = BetterLaunch()
 
@@ -15,5 +15,5 @@ def stop_bugging_me():
         "publisher_member_function",
         "my_talker",
         # Strings with quoted sections will be split correctly thanks to shlex
-        exec_args="xterm -e gdb -ex run --args",
+        prefix_args="xterm -e gdb -ex run --args",
     )
